57c4c7fabcdc9e992c3b5372d2a3de0c00320883
heshan <heshan@heshan-ThinkPad-T520.(none)>
2013-05-01 18:37:58 -0400

    Commenting the unused methods.

--- flask_airavata/src/server.py
+++ flask_airavata/src/server.py
@@ -1,90 +1,64 @@
+##
+## Server logic for the web-app is contained here. 
+##
+
+
 from flask import Flask, render_template, request
 app = Flask(__name__)
 
+
 dict = {}
+
 
 @app.route('/')
 def home():
   return render_template('fluid.html')
 
+
 @app.route('/about')
 def about():
   return render_template('about.html')
+
 
 @app.route('/ack')
 def ack():
   return render_template('ack.html')
 
+
 @app.route('/contact')
 def contact():
   return render_template('contact.html')
+
 
 @app.route('/why')
 def why():
   return render_template('why.html')
 
+
 @app.route('/configuring')
 def configuring():
   return render_template('configuring.html')
+
 
 @app.route('/configure_base')
 def configure_base():
   return render_template('configure_base.html')
 
+
 @app.route('/configure_custom')
 def configure_custom():
   return render_template('configure_custom.html')
+
 
 @app.route('/monitoring')
 def monitoring():
   return render_template('monitoring.html')
 
+
 @app.route('/create')
 def create():
   return render_template('create.html')
 
-def remoteConnection(id):
-# from gregor
-
-  result = fgrep(nova.show(id), 'network')
-
-  (start, label, ips, rest) = result.replace(' ', '').split('|')
-  print
-  ips
-  try:
-    (private_ip, public_ip) = ips.split(',')
-  except:
-    print 'public IP is not yet assigned'
-    sys.exit()
-  print public_ip
-  rsh = ssh.bake('%s@%s' % ('ubuntu', str(public_ip)))
-  remote = rsh.bake('-o StrictHostKeyChecking no')
-  test1 = remote('uname').replace('\n', '')
-  print '<%s>' % test1
-  print remote('uname -a')
-  print remote('hostname')
-  print remote('pwd')
-
-def boot(index):
-  '''starts a virtual machine with the given index'''
-
-  try:  
-    #number = str(index).zfill(3)
-    #name = '%s-%s' % (prefix, number)
-    name = "airavata-cloud-image"
-    print 'Launching VM %s' % name
-    tmp = nova(
-        'boot',
-        '--flavor=1',
-        '--image=%s' % image_name,
-        '--key_name',
-        '%s' % prefix,
-        '%s' % name,
-        )
-    print tmp
-  except Exception, e:
-    print e
-    print 'Failure to launch %s' % name    
 
 @app.route('/image-result', methods=['GET', 'POST'])
 def imageResult():  
@@ -132,10 +106,56 @@ def imageResult():
   
   return render_template('image-result.html')
 
+
 def printKeys(myKeys):
   print "Printing keys ... "
   for key in myKeys:
     print """<p>key: %s, value: %s type: %s</p>"""%(key, form[key].value, type(form[key].value)) 
 
+
 if __name__ == '__main__':
   app.run(debug=True)
+
+
+
+##def remoteConnection(id):
+##  result = fgrep(nova.show(id), 'network')
+##
+##  (start, label, ips, rest) = result.replace(' ', '').split('|')
+##  print
+##  ips
+##  try:
+##    (private_ip, public_ip) = ips.split(',')
+##  except:
+##    print 'public IP is not yet assigned'
+##    sys.exit()
+##  print public_ip
+##  rsh = ssh.bake('%s@%s' % ('ubuntu', str(public_ip)))
+##  remote = rsh.bake('-o StrictHostKeyChecking no')
+##  test1 = remote('uname').replace('\n', '')
+##  print '<%s>' % test1
+##  print remote('uname -a')
+##  print remote('hostname')
+##  print remote('pwd')
+##
+##def boot(index):
+##  '''starts a virtual machine with the given index'''
+##
+##  try:  
+##    #number = str(index).zfill(3)
+##    #name = '%s-%s' % (prefix, number)
+##    name = "airavata-cloud-image"
+##    print 'Launching VM %s' % name
+##    tmp = nova(
+##        'boot',
+##        '--flavor=1',
+##        '--image=%s' % image_name,
+##        '--key_name',
+##        '%s' % prefix,
+##        '%s' % name,
+##        )
+##    print tmp
+##  except Exception, e:
+##    print e
+##    print 'Failure to launch %s' % name    
+##
