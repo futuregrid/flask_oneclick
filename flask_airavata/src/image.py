ecb58e98b95e506be8d2759bc9988f05ab5986b5
heshan <heshan@heshan-ThinkPad-T520.(none)>
2013-05-06 10:33:51 -0400

    Updating the image creation htmls.

--- flask_airavata/src/image.py
+++ flask_airavata/src/image.py
@@ -1,3 +1,8 @@
+## 
+## Creates an image in the FutureGrid environement with the given input data
+##
+
+
 import sys
 sys.path.insert(0, './')
 sys.path.insert(0, '../')
@@ -11,45 +16,45 @@ from cloudmesh.cloudmesh import cloudmesh
 from datetime import datetime
 from cloudmesh.cm_config import cm_config
 from datetime import datetime
-try:
-    from sh import xterm
-except:
-    print "xterm not suppported"
-import yaml
 
 
-print "Start executing the script" 
+class CloudConfigManager():
 
-#### setting up reading path for the use of yaml################
-default_path = '.futuregrid/cloudmesh.yaml'
-home = os.environ['HOME']
-filename = "%s/%s" % (home, default_path)
+    def start_image(self, args):
+        print "Generating an image in the FutureGrid environement"
 
-#### end of setting up reading path for the use of yaml################	
+        cloud = args['cloud']
+        username = args['username']
+        userId = args['userId']
+        instanceId = args['instanceId']
+        imageSize = args['imageSize']
+        publicKey = args['publicKey']
 
-cloud = "grizzly-openstack"
-clouds = cloudmesh()
-clouds.refresh()
-d = clouds.configuration
-print d.clouds
+        # setting up reading path for the use of yaml
+        default_path = '.futuregrid/cloudmesh.yaml'
+        home = os.environ['HOME']
+        filename = "%s/%s" % (home, default_path)
+        
+        clouds = cloudmesh()
+        clouds.refresh()
+        #d = clouds.configuration
+        #id = clouds.create(cloud, username, userId, instanceId, imageSize, publicKey)
+        #print "image id : ", id
 
-#sys.exit()
-
-
-print clouds.get()
-#image = d.get()["cloudmesh"]["clouds"][cloud]["image"]
-#print image 
-#sys.exit()
-#print clouds.configuration
-
-
-print d.userkeys("default")
+        print clouds.clouds['india-openstack']['servers']['f1c8e310-282c-4572-a605-129e32bc76ae']
 
 
-key = d.userkeys("default")
-print key
-#sys.exit()
-# cloud_name, prefix, index, image_id, flavor_name, key
-clouds.create(cloud, "heshan", "1", "ktanaka/ubuntu1204-ramdisk.manifest.xml", "openstack", "/N/u/heshan/.futuregrid/openstack/pk.pem")
+if __name__ == '__main__':
+    args = {}
+    args['cloud'] = "india-openstack"
+    args['username'] = "heshan"
+    args['userId'] = "1"
+    args['instanceId'] = "6d2bca76-8fff-4d57-9f29-50378539b4fa"
+    args['imageSize'] = "m1.tiny"
+    args['publicKey'] = "heshan-key"
+    
+    manager = CloudConfigManager()
+    manager.start_image(args)
 
 
+    
